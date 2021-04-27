28fa4675f34e3be068ad53ae321a6a7818a9ebeb
fyx <iws_fyx@163.com>
2021-04-27 20:03:06 +0800

    博客提交

--- blog/src/main/java/com/program/blog/config/WebConfig.java
+++ blog/src/main/java/com/program/blog/config/WebConfig.java
@@ -9,6 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/admin/**").excludePathPatterns("/admin").excludePathPatterns("/admin/login");
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/admin/**").excludePathPatterns("/admin").excludePathPatterns("/admin/login");
     }
 }
